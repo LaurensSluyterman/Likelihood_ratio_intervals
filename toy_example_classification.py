fdbb202ec7b2bb208ef543ceb8e6fc596e2ddac5
LaurensSluyterman <Laurens.sluyterman@hotmail.com>
2023-02-06 17:12:55 +0100

    changed plotting

--- toy_example_classification.py
+++ toy_example_classification.py
@@ -6,21 +6,28 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from intervals_classification import CI_classificationx
 from scipy import stats
-from utils import sigmoid
-
+from utils import
+import matplotlib
+matplotlib.use("TkAgg")
+plt.rcParams['text.usetex'] = True
+plt.rcParams["font.size"] = 17
+plt.rcParams['axes.linewidth'] = 0.2
 #%% Generate a data set
 def p(x):
     return 0.5 + 0.4*np.cos(6*x)
 
 
 np.random.seed(2)
-n = 50
+n = 30
 x_train = np.hstack((np.random.uniform(0, 0.2, n), np.random.uniform(0.8, 1, n)))
 x_train = np.reshape(x_train, (len(x_train), 1))
 y_train = np.random.binomial((1), p=p(x_train))[:, 0]
 x_lin = np.linspace(0, 1, 50)
-plt.plot(x_train, y_train, 'o')
+plt.plot(x_train, y_train, 'o', label='y-values')
 plt.plot(np.linspace(0, 1, 50), p(np.sort(np.linspace(0, 1, 50))))
+plt.xlabel(r'$x$')
+plt.ylabel(r'$p(x)$')
+plt.tight_layout()
 plt.show()
 
 
@@ -28,13 +35,13 @@ plt.show()
 def get_model():
     model = Sequential()
     model.add(Dense(30, activation='elu', input_shape=(1, ),
-                    kernel_regularizer=tf.keras.regularizers.l2(l=0.00)))
+                    kernel_regularizer=tf.keras.regularizers.l2(l=0.0001)))
     model.add(Dense(30, activation='elu',
-                    kernel_regularizer=tf.keras.regularizers.l2(l=0.00)))
+                    kernel_regularizer=tf.keras.regularizers.l2(l=0.0001)))
     model.add(Dense(30, activation='elu',
-                    kernel_regularizer=tf.keras.regularizers.l2(l=0.00)))
+                    kernel_regularizer=tf.keras.regularizers.l2(l=0.0001)))
     model.add(Dense(1,
-                    kernel_regularizer=tf.keras.regularizers.l2(l=0.00)))
+                    kernel_regularizer=tf.keras.regularizers.l2(l=0.0001)))
     model.compile(optimizer='adam',
                   loss=tf.keras.losses.BinaryCrossentropy(from_logits=True, label_smoothing=0),
                   metrics=['accuracy'])
@@ -45,9 +52,12 @@ def get_model():
 
 model = get_model()
 plt.plot(x_train, y_train, 'o')
-plt.plot(np.linspace(0, 1, 50), p(np.sort(np.linspace(0, 1, 50))), label='p(x)')
-plt.plot(x_lin, tf.math.sigmoid(model.predict(x_lin)), label='p_hat(x)')
-plt.legend()
+plt.plot(np.linspace(0, 1, 50), p(np.sort(np.linspace(0, 1, 50))), label=r'$p(x)$')
+plt.plot(x_lin, tf.math.sigmoid(model.predict(x_lin)), label=r'$\hat{p}(x)$')
+plt.legend(loc='center', bbox_to_anchor=(0.5, 1.1), ncol=2)
+plt.xlabel(r'$x$')
+plt.ylabel(r'$p(x)$')
+plt.tight_layout()
 plt.show()
 
 #%% A single prediction
@@ -56,7 +66,7 @@ CI_classificationx(model=model, x=np.array([0.1]), X_train=x_train,
                    n_steps=100, alpha=0.05, n_epochs=2, fraction=0.2)
 
 #%% Creating CIs for a test set
-x_lin = np.linspace(-0.8, 1.8, 30)
+x_lin = np.linspace(-0.8, 1.8, 50)
 CIs = np.zeros((len(x_lin), 2))
 for i, x in enumerate(x_lin):
     print(i+1)
@@ -65,21 +75,23 @@ for i, x in enumerate(x_lin):
                                               X_train=x_train,
                                               Y_train=y_train,
                                               p_hats=model.predict(x_train),
-                                              n_steps=100,
-                                              alpha=0.2,
+                                              n_steps=150,
+                                              alpha=0.1,
                                               n_epochs=150,
                                               fraction=0.2,
                                               verbose=0)
 #%% Visualizing the results
 p_hat = tf.math.sigmoid(model.predict(x_lin))
 plt.plot(x_train, y_train, 'o')
-plt.plot(x_lin, p(x_lin), label='p(x)')
-plt.plot(x_lin, p_hat, label='p_hat(x)')
-plt.fill_between(x_lin, CIs[:, 0], CIs[:, 1], color='blue', alpha=0.2, linewidth=0.1, label='CI')
-plt.xlabel('x')
-plt.legend()
+plt.plot(x_lin, p(x_lin), label=r'$p(x)$')
+plt.plot(x_lin, p_hat, label=r'$\hat{p}(x)$')
+plt.fill_between(x_lin, CIs[:, 0], CIs[:, 1], color='blue', alpha=0.2, linewidth=0.1, label=r'CI')
+plt.legend(loc='center', bbox_to_anchor=(0.5, 1.1), ncol=3)
+plt.xlabel(r'$x$')
+plt.ylabel(r'$p(x)$')
+plt.tight_layout()
 plt.show()
-
+plt.close()
 #%% Creating CIs using an ensemble
 ensemble = [get_model() for _ in range(10)]
 CI_ensemble = np.zeros((len(x_lin), 2))
