f1cc2c9287dd9349a21ae3c2cdbd2547d9de454b
LaurensSluyterman <Laurens.sluyterman@hotmail.com>
2023-02-21 13:40:23 +0100

    changed training process

--- intervals_classification.py
+++ intervals_classification.py
@@ -1,4 +1,3 @@
-
 import tensorflow as tf
 import numpy as np
 from tensorflow.keras.models import Sequential
@@ -7,47 +6,53 @@ from utils import sigmoid
 
 
 def CI_classificationx(*, model, x, X_train, Y_train, p_hats,
-                       n_steps, alpha, n_epochs, fraction,
-                       verbose=True, from_sigmoid=True):
+                       n_steps, alpha, n_epochs, fraction, weight,
+                       compile=False, optimizer='Adam', verbose=True, from_sigmoid=True):
     # Copying the model
     model.save('./models/tempmodel.h5', overwrite=True)
     alternative_network_positive = tf.keras.models.load_model('./models/tempmodel.h5')
     alternative_network_negative = tf.keras.models.load_model('./models/tempmodel.h5')
 
+    # Recompile the model if desired (to reset the optimizer state)
+    if compile:
+        alternative_network_positive.compile(optimizer=optimizer,
+              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+        alternative_network_negative.compile(optimizer=optimizer,
+                                             loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                                             metrics=['accuracy'])
+
     # Letting the networks train on to get close to a 0 and 1 prediction
     x = np.array([x])
-    start_value = model.predict(x)
-    if not from_sigmoid:
-        start_value = sigmoid(model.predict(x))
+    start_value = model.predict(x, verbose=0)
+
+
     N = len(X_train)
+    N_extra = int(N * fraction)
     x = np.reshape(x, np.shape(X_train[0]))
-    x_multiple_copies = np.array([x for _ in range(np.int(N * fraction))])
-    positive_labels = np.array([1 for _ in range(np.int(N * fraction))])
-    negative_labels = np.array([0 for _ in range(np.int(N * fraction))])
+    x_multiple_copies = np.array([x for _ in range(N_extra)])
+    positive_labels = np.array([1 for _ in range(N_extra)])
+    negative_labels = np.array([0 for _ in range(N_extra)])
     probs = tf.math.sigmoid(p_hats)[:, 0].numpy()
-    # We repeat the targets by the predictions of the model to prevent overfitting
+    # probs = [sigmoid(p)[0] for p in p_hats]
+    # We replace the targets by the predictions of the model to prevent overfitting
     Y_train_positive = np.hstack((probs, positive_labels))
     Y_train_negative = np.hstack((probs, negative_labels))
+    sample_weights = np.hstack((np.ones(len(probs)), weight / N_extra * np.ones(N_extra)))
     X_train_new = np.vstack((X_train, x_multiple_copies))
     alternative_network_positive.fit(X_train_new, Y_train_positive, epochs=n_epochs,
-                                     verbose=verbose, batch_size=None)
+                                     verbose=verbose, batch_size=int(32*(1+fraction)),
+                                     sample_weight=sample_weights)
     alternative_network_negative.fit(X_train_new, Y_train_negative, epochs=n_epochs,
-                                     verbose=verbose, batch_size=None)
+                                     verbose=verbose, batch_size=int(32*(1+fraction)),
+                                     sample_weight=sample_weights)
 
     # The maximum and minimum achieved probability predictions
-    max_value = alternative_network_positive.predict(np.array([x]))
-    min_value = alternative_network_negative.predict(np.array([x]))
+    max_value = alternative_network_positive.predict(np.array([x]), verbose=0)
+    min_value = alternative_network_negative.predict(np.array([x]), verbose=0)
 
-    perturbed_predictions_positive = alternative_network_positive.predict(X_train)
-    perturbed_predictions_negative = alternative_network_negative.predict(X_train)
-
-    # If interpolating after the sigmoid, we transform everything to probabilities
-    if not from_sigmoid:
-        perturbed_predictions_positive = tf.math.sigmoid(perturbed_predictions_positive)[:, 0].numpy()
-        perturbed_predictions_negative = tf.math.sigmoid(perturbed_predictions_negative)[:, 0].numpy()
-        max_value = sigmoid(alternative_network_positive.predict(np.array([x])))
-        min_value = sigmoid(alternative_network_negative.predict(np.array([x])))
-        p_hats = probs
+    perturbed_predictions_positive = alternative_network_positive.predict(X_train, verbose=0)
+    perturbed_predictions_negative = alternative_network_negative.predict(X_train, verbose=0)
 
     # Checking which alternatives still get accepted
     Y_train_reshaped = np.reshape(Y_train, (len(Y_train), 1))
@@ -69,7 +74,7 @@ def CI_classificationx(*, model, x, X_train, Y_train, p_hats,
 
             # A stop criterium when the upper or lowerbound reaches 1 or 0.
             if l > 1:
-                if 0.999 < sigmoid(upperbound) or sigmoid(upperbound) < 0.0001:
+                if 0.999 < sigmoid(upperbound):
                     accepting = 0
             l += 1 / n_steps
 
@@ -86,20 +91,19 @@ def CI_classificationx(*, model, x, X_train, Y_train, p_hats,
             lowerbound = start_value * (1-l) + l * min_value
             # A stop criterium when the upper or lowerbound reaches 1 or 0.
             if l>1:
-                if 0.999 < sigmoid(lowerbound) or sigmoid(lowerbound) < 0.0001:
+                if sigmoid(lowerbound) < 0.0001:
                     accepting = 0
             l += 1 / n_steps
 
     # Deleting the new networks to alleviate some memory issues
-    del alternative_network_positive, alternative_network_negative
     if from_sigmoid:
         lowerbound = sigmoid(lowerbound)
         upperbound = sigmoid(upperbound)
     return lowerbound[0][0], upperbound[0][0]
 
 
-def accept_LR_classification(Y_train, p_tildes, p_hat, alpha, from_sigmoid):
-    log_likelihood_difference = loglikelihood(Y_train, p_tildes, from_sigmoid) - loglikelihood(Y_train, p_hat, from_sigmoid)
+def accept_LR_classification(Y_train, p_tildes, p_hat, alpha):
+    log_likelihood_difference = loglikelihood(Y_train, p_tildes) - loglikelihood(Y_train, p_hat)
     if log_likelihood_difference < np.log(alpha):
         return 0
     else:
@@ -108,41 +112,8 @@ def accept_LR_classification(Y_train, p_tildes, p_hat, alpha, from_sigmoid):
 
 bce = tf.keras.losses.BinaryCrossentropy(from_logits=True,
                                          reduction=tf.keras.losses.Reduction.NONE,
-                                         label_smoothing=0)
-def loglikelihood(y, p, from_sigmoid=True):
+                                         label_smoothing=0.00)
+def loglikelihood(y, p):
     loglik = -bce(y, p)
     return np.sum(loglik)
 
-
-
-
-# np.exp(loglikelihood(Y_train_reshaped, p_tildes)
-#                                - loglikelihood(Y_train_reshaped, p_hats))
-#
-# sigmoid(upperbound)
-# sigmoid(start_value)
-#
-# plt.plot(history.history['accuracy'])
-# plt.show()
-#
-#
-# [bce(Y_train_reshaped, l/10 * perturbed_predictions_positive + (1-l/10)*p_hats)[20].numpy() for l in range(0, 11)]
-# [loglikelihood(Y_train_reshaped, l/10 * perturbed_predictions_negative + (1-l/10)*p_hats) for l in range(0, 11)]
-# [loglikelihood(Y_train_reshaped, l/10 * perturbed_predictions_positive + (1-l/10)*p_hats) for l in range(0, 11)]
-#
-# y_true = [0, 1, 0, 0]
-# y_pred = [-18.6, 0.51, 2.94, -12.8]
-# bce(y_true, y_pred).numpy()
-#
-# model.evaluate(X_train, Y_train)
-# def loglikelihood(y, p, from_sigmoid=False):
-#     ll = 0
-#     for i, label in enumerate(y):
-#         prob = p[i]
-#         if from_sigmoid:
-#             prob = sigmoid(prob)
-#         if label == 0:
-#             ll += np.log(1-p)
-#         if label == 1:
-#             ll += np.log(p)
-#     return ll
