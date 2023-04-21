b093e7ed4d58b8022d1fcf33a3cc6ef1f74c4e6b
LaurensSluyterman <Laurens.sluyterman@hotmail.com>
2023-04-21 12:16:12 +0200

    Small clean-up

--- cifar_example.py
+++ cifar_example.py
@@ -2,23 +2,18 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, Dropout, AveragePooling2D
 from keras.datasets import cifar10
 from adverserial_example_generation import generate_adversarial_example
 from intervals_classification import CI_classificationx
 
-#%% Relaod
-from importlib import reload
-import intervals_classification
-reload(intervals_classification)
-from intervals_classification import CI_classificationx
 
 #%% Data import and preprocessing
 data = cifar10.load_data()
 
 (x_train, y_train), (x_test, y_test) = data
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
+x_train = x_train.astype('float64')
+x_test = x_test.astype('float64')
 
 x_train /= 255.
 x_test /= 255.
@@ -41,83 +36,92 @@ y_train01 = np.hstack((y_train_0, y_train_1))
 
 x_test01 = np.vstack((x_test_0, x_test_1))
 y_test01 = np.hstack((y_test_0, y_test_1))
-#%%
+
+#%% Visual check
 plt.imshow(x_train_7[4])
 plt.show()
 
 #%% Training a model
 input_shape = (32, 32, 3)
 c_reg = 1e-5
-
+batch_size = 32
+n_epochs = 15
+optimizer = 'sgd'
+np.random.seed(2)
 model = Sequential()
-model.add(Conv2D(32, kernel_size=(3,3), input_shape=input_shape,
-                 kernel_regularizer=tf.keras.regularizers.l2(l=c_reg)))
+model.add(Conv2D(32, kernel_size=(3,3),
+                 input_shape=input_shape, activation='elu',
+                 kernel_regularizer=tf.keras.regularizers.l2(l=0)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(64, kernel_size=(3,3), activation='relu',
-                 kernel_regularizer =tf.keras.regularizers.l2(l=c_reg)))
+model.add(Conv2D(32, kernel_size=(3,3), activation='elu',
+                 kernel_regularizer=tf.keras.regularizers.l2(l=0)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(64, kernel_size=(3,3), activation='relu',
-                 kernel_regularizer =tf.keras.regularizers.l2(l=c_reg)))
 model.add(Flatten())
-model.add(Dense(30, activation='relu',
+model.add(Dense(30, activation='elu',
                 kernel_regularizer=tf.keras.regularizers.l2(l=c_reg)))
-model.add(Dense(30, activation='relu',
+model.add(Dense(30, activation='elu',
                 kernel_regularizer=tf.keras.regularizers.l2(l=c_reg)))
-model.add(Dense(30, activation='relu',
+model.add(Dense(30, activation='elu',
                 kernel_regularizer=tf.keras.regularizers.l2(l=c_reg)))
 model.add(Dense(1,
                 kernel_regularizer=tf.keras.regularizers.l2(l=c_reg)))
 
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True, label_smoothing=0),
+model.compile(optimizer=optimizer,
+              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-model.fit(x=x_train01, y=y_train01, epochs=5)
+model.fit(x=x_train01, y=y_train01, epochs=n_epochs, batch_size=batch_size, validation_split=0)
 
-#%% Calculating confidence intervals
-model = tf.keras.models.load_model('./models/CNNcifar')
-print(CI_classificationx(model=model, x=x_train_4[0], X_train=x_train01,
-                          Y_train=y_train01, p_hats=model.predict(x_train01),
-                          n_steps=150, alpha=0.1, n_epochs=3, fraction=0.1,
-                         verbose=0, from_sigmoid=True))
-
-print(CI_classificationx(model=model, x=x_test01[-2], X_train=x_train01,
-                          Y_train=y_train01, p_hats=model.predict(x_train01),
-                          n_steps=150, alpha=0.1, n_epochs=3, fraction=0.1,
-                         verbose=0, from_sigmoid=True))
-
-print(CI_classificationx(model=model, x=x_train01[-2], X_train=x_train01,
-                          Y_train=y_train01, p_hats=model.predict(x_train01),
-                          n_steps=150, alpha=0.1, n_epochs=3, fraction=0.1,
-                         verbose=0, from_sigmoid=True))
 
 #%%
-plt.imshow(x_train_4[0])
-plt.show()
-#%% Adversarial example
-model = tf.keras.models.load_model('./models/CNNcifar')
+model.save('./models/CNNcifar-1e-5-sgd-n15')
+#%% Calculating confidence intervals
+model = tf.keras.models.load_model('./models/CNNcifar-1e-4-sgd')
 
-x_normal = x_test01[-1]
-label = np.array([y_test01[-1]])
+print(CI_classificationx(model=model, x=x_train01[-1], X_train=x_train01,
+                         Y_train=y_train01, predicted_logits=model.predict(x_train01),
+                         n_steps=200, alpha=0.1, n_epochs=n_epochs, fraction=2/batch_size,
+                         verbose=1, optimizer=optimizer,
+                         weight=1))
+
+print(CI_classificationx(model=model, x=x_test01[-1], X_train=x_train01,
+                         Y_train=y_train01, predicted_logits=model.predict(x_train01),
+                         n_steps=200, alpha=0.1, n_epochs=n_epochs, fraction=2/batch_size,
+                         verbose=1, optimizer=optimizer, compile=True,
+                         batch_size=batch_size, weight=1))
+
+print(CI_classificationx(model=model, x=x_train_4[0], X_train=x_train01,
+                         Y_train=y_train01, predicted_logits=model.predict(x_train01),
+                         n_steps=200, alpha=0.1, n_epochs=n_epochs, fraction=2/batch_size,
+                         verbose=1, compile=True, optimizer=optimizer,
+                         batch_size=batch_size, weight=1))
+
+
+#%% Display an image
+plt.imshow(x_test01[-1])
+plt.show()
+
+#%% Adversarial example
+model = tf.keras.models.load_model('./models/CNNcifar-1e-5-sgd')
+
+x_normal = x_test01[-2]
+label = np.array([y_test01[-2]])
 x_adversarial = generate_adversarial_example(model, np.array([x_normal]),
-                                             label, epsilon=0.02)
+                                             label, epsilon=0.03)
 
 print(CI_classificationx(model=model, x=x_normal, X_train=x_train01,
-                          Y_train=y_train01, p_hats=model.predict(x_train01),
-                          n_steps=150, alpha=0.1, n_epochs=3, fraction=0.1,
-                         verbose=0, from_sigmoid=True))
+                         Y_train=y_train01, predicted_logits=model.predict(x_train01), optimizer=optimizer,
+                         n_steps=200, alpha=0.1, n_epochs=n_epochs, fraction=2/batch_size,
+                         verbose=1, weight=1, compile=True))
 
 print(CI_classificationx(model=model, x=x_adversarial[0], X_train=x_train01,
-                          Y_train=y_train01, p_hats=model.predict(x_train01),
-                          n_steps=150, alpha=0.1, n_epochs=3, fraction=0.1,
-                         verbose=0, from_sigmoid=True))
+                         Y_train=y_train01, predicted_logits=model.predict(x_train01),
+                         n_steps=200, alpha=0.1, n_epochs=n_epochs, optimizer=optimizer
+                         , fraction=2/batch_size,
+                         verbose=1, weight=1))
 
 
-
-print(model.predict(np.array([x_normal])))
-print(model.predict(x_adversarial))
-
-# This code snippet was made by chat GPT.
+# Display original and adversarial input. This code snippet was made by chatGPT.
 f, ax = plt.subplots(1, 2)
 ax[0].imshow(x_normal.squeeze())
 ax[0].set_title('Original Input')
