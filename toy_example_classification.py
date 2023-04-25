40877c23846975c86c0cd6882ed5589a18651d2a
LaurensSluyterman <Laurens.sluyterman@hotmail.com>
2023-04-25 12:53:40 +0200

    combined get_model and get_model_dropout functions

--- toy_example_classification.py
+++ toy_example_classification.py
@@ -4,7 +4,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import scipy
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense, Dropout
 from intervals_classification import CI_classificationx
 from scipy import stats
 from utils import sigmoid
@@ -34,17 +34,19 @@ plt.show()
 
 
 #%% Train a model and visualize the predictions
-c_reg = 1e-4
+c_reg = 1e-5
 n_epochs = 300
 
 
-def get_model():
+def get_model(c_reg, n_epochs, dropout_rate=0):
     """Train simple network with three hidden layers"""
     model = Sequential()
     model.add(Dense(30, activation='elu', input_shape=(1, ),
                     kernel_regularizer=tf.keras.regularizers.l2(l=c_reg)))
+    model.add(Dropout(dropout_rate))
     model.add(Dense(30, activation='elu',
                     kernel_regularizer=tf.keras.regularizers.l2(l=c_reg)))
+    model.add(Dropout(dropout_rate))
     model.add(Dense(30, activation='elu',
                     kernel_regularizer=tf.keras.regularizers.l2(l=c_reg)))
     model.add(Dense(1,
@@ -56,7 +58,7 @@ def get_model():
     return model
 
 
-model = get_model()
+model = get_model(c_reg, n_epochs)
 plt.plot(x_train, y_train, 'o')
 plt.plot(np.linspace(0, 1, 50), p(np.sort(np.linspace(0, 1, 50))), label=r'$p(x)$')
 plt.plot(x_lin, tf.math.sigmoid(model.predict(x_lin)), label=r'$\hat{p}(x)$')
@@ -67,23 +69,23 @@ plt.tight_layout()
 plt.show()
 
 #%% A single prediction
-CI_classificationx(model=model, x=np.array([0]), X_train=x_train,
+CI_classificationx(model=model, x=np.array([0.5]), X_train=x_train,
                    Y_train=y_train, predicted_logits=model.predict(x_train),
-                   n_steps=100, alpha=0.1, n_epochs=500, fraction=1/16, weight=1)
+                   n_steps=100, alpha=0.05, n_epochs=300, fraction=1/16, weight=1)
 
 #%% Creating CIs for a test set
 x_lin = np.linspace(-0.8, 1.8, 50)
 CIs = np.zeros((len(x_lin), 2))
-p_hats = model.predict(x_train)
+predicted_logits = model.predict(x_train)
 for i, x in enumerate(x_lin):
     print(i+1)
     CIs[i, 0], CIs[i, 1] = CI_classificationx(model=model,
                                               x=x_lin[i],
                                               X_train=x_train,
                                               Y_train=y_train,
-                                              predicted_logits=p_hats,
+                                              predicted_logits=predicted_logits,
                                               n_steps=100,
-                                              alpha=0.1,
+                                              alpha=0.05,
                                               n_epochs=n_epochs,
                                               fraction=1/16,
                                               verbose=0,
@@ -91,8 +93,9 @@ for i, x in enumerate(x_lin):
 
 #%% Visualizing the results
 p_hat = tf.math.sigmoid(model.predict(x_lin))
+plt.figure(dpi=200)
 plt.plot(x_train, y_train, 'o')
-plt.plot(x_lin, p(x_lin), label=r'$p(x)$')
+plt.plot(x_lin, p(x_lin), linestyle='--', label=r'$p(x)$')
 plt.plot(x_lin, p_hat, label=r'$\hat{p}(x)$')
 plt.fill_between(x_lin, CIs[:, 0], CIs[:, 1], color='blue', alpha=0.2, linewidth=0.1, label=r'CI')
 plt.legend(loc='center', bbox_to_anchor=(0.5, 1.1), ncol=3)
@@ -103,12 +106,13 @@ plt.show()
 plt.close()
 
 #%% Creating CIs using an ensemble
-ensemble = [get_model() for _ in range(10)]
+ensemble = [get_model(c_reg=1e-5, n_epochs=300) for _ in range(10)]
 CI_ensemble = np.zeros((len(x_lin), 2))
 p_hats_ensemble = np.zeros((len(x_lin)))
+alpha = 0.05
 for i, x in enumerate(x_lin):
     print(i+1)
-    t = scipy.stats.t(df=9).ppf(1-0.1/2)
+    t = scipy.stats.t(df=len(ensemble)-1).ppf(1-alpha/2)
     predictions = [sigmoid(model.predict(np.array([x]))) for model in ensemble]
     var = np.var(predictions)
     mean = np.mean(predictions)
@@ -117,10 +121,36 @@ for i, x in enumerate(x_lin):
     p_hats_ensemble[i] = mean
 
 #%% Visualizing the ensemble results
+plt.figure(dpi=200)
 plt.plot(x_train, y_train, 'o')
-plt.plot(x_lin, p(x_lin), label=r'$p(x)$')
+plt.plot(x_lin, p(x_lin),linestyle='--', label=r'$p(x)$')
 plt.plot(x_lin, p_hats_ensemble, label=r'$\hat{p}(x)$')
 plt.fill_between(x_lin, CI_ensemble[:, 0], CI_ensemble[:, 1], color='blue', alpha=0.2, linewidth=0.1, label=r'CI')
+plt.legend(loc='center', bbox_to_anchor=(0.5, 1.1), ncol=3)
+plt.xlabel(r'$x$')
+plt.ylabel(r'$p(x)$')
+plt.tight_layout()
+plt.show()
+
+#%% Creating CIs using dropout
+model = get_model(c_reg=1e-5, n_epochs=300, dropout_rate=0.2)
+
+B = 1000  # Amount of forward passes
+CI_dropout = np.zeros((len(x_lin), 2))
+p_hats_dropout = np.zeros((len(x_lin)))
+for i, x in enumerate(x_lin):
+    print(i)
+    predictions = [sigmoid(model(np.array([x]), training=1)) for _ in range(B)]
+    prediction = np.mean(predictions)
+    CI_dropout[i, 0] = np.percentile(predictions, 2.5)
+    CI_dropout[i, 1] = np.percentile(predictions, 97.5)
+    p_hats_dropout[i] = prediction
+#%% Visualizing the ensemble results
+plt.figure(dpi=200)
+plt.plot(x_train, y_train, 'o')
+plt.plot(x_lin, p(x_lin), linestyle='--', label=r'$p(x)$')
+plt.plot(x_lin, p_hats_dropout, label=r'$\hat{p}(x)$')
+plt.fill_between(x_lin, CI_dropout[:, 0], CI_dropout[:, 1], color='blue', alpha=0.2, linewidth=0.1, label=r'CI')
 plt.legend(loc='center', bbox_to_anchor=(0.5, 1.1), ncol=3)
 plt.xlabel(r'$x$')
 plt.ylabel(r'$p(x)$')
