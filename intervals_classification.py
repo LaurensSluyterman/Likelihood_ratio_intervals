aeff35de6218638a11aa5bffc847596e44083efe
LaurensSluyterman <Laurens.sluyterman@hotmail.com>
2023-07-25 13:48:32 +0200

    Added ensemble/dropout and docstring

--- intervals_classification.py
+++ intervals_classification.py
@@ -4,10 +4,41 @@ import scipy
 from utils import sigmoid
 from scipy import stats
 
+
 def CI_classificationx(*, model, x, X_train, Y_train, predicted_logits,
                        n_steps, alpha, n_epochs, fraction, weight=1,
                        compile=False, optimizer='Adam', verbose=True,
                        batch_size=32):
+    """Create a confidence interval for a binary classification problem.
+
+    Arguments:
+        model: The original model trained on (X_train, Y_train).
+        x: The new input for which a CI is constructed.
+        X_train: The training features.
+        Y_train: The training targets.
+        predicted_logits: The predictions of the model on the training data.
+        n_steps (int): Determines the step size when creating the CI.
+            if the predicted logit is 0.8 and 0.99 after retraining, then
+            the step size will be 0.19/100.
+        alpha (float): Determines the confidence level of the CI. An alpha of
+            0.1 corresponds to a 90% CI.
+        n_epochs (int): The number of training epochs
+        fraction (float): Determines how many new data points are added to train
+            the perturbed networks. If this is set to 1/16 and the training data
+            set has 2000 data points, then 2000/16 points are added.
+        weight (float): The weight that these newly added points get in the
+            loss contribution. If set to 1, the sum of the contributions
+            of the newly added points adds to 1. If 15 points are added, each
+            loss contribution will in this case be divided by 15.
+        compile (Boolean): Determines if we recompile the network before
+            retraining to find the perturbed networks.
+        optimizer: The optimizer of the network
+        verbose (Boolean): Determines if the training progress is displayed.
+        batch_size (int): The batch size during training.
+
+    Return:
+        lowerbound, upperbound: The lower- and upperbound of the CI.
+    """
     # Copying the model
     model.save('./models/tempmodel.h5', overwrite=True)
     alternative_network_positive = tf.keras.models.load_model('./models/tempmodel.h5')
@@ -96,6 +127,7 @@ def CI_classificationx(*, model, x, X_train, Y_train, predicted_logits,
 
 
 def accept_LR_classification(Y_train, perturbed_logits, predicted_logits, alpha):
+    """Check if the likelihood ratio exceeds the critical value."""
     log_likelihood_difference = loglikelihood(Y_train, predicted_logits) \
                                 - loglikelihood(Y_train, perturbed_logits)
     critical_value = stats.chi2(1).ppf(1-alpha)
@@ -121,3 +153,56 @@ def loglikelihood(y, logits):
     """
     loglik = -bce(y, logits)
     return np.sum(loglik)
+
+
+def CI_ensemble(ensemble, x, alpha=0.05, give_prediction=True):
+    """Construct a CI using an ensemble.
+
+    Arguments:
+        ensemble: A list containing the ensemble members
+        x: The input for which a CI is constructed.
+        B (int): The number of forward passes that is used.
+        alpha (float): The confidence level of the CI. An alpha of 0.05 corresponds
+            to a 95% CI.
+        give_prediction (Boolean): Determines if the average of the predictions
+            is also returned.
+    Return:
+        prediction: The average of the predictions of the ensemble members
+        lowerbound, upperbound: The lower- and upperbound of the CI.
+    """
+    predictions = [sigmoid(model.predict(np.array([x]), verbose=0)) for model in ensemble]
+    var = np.var(predictions)
+    prediction = np.mean(predictions)
+    t = scipy.stats.t(df=len(ensemble)-1).ppf(1 - alpha / 2)
+    lowerbound = np.max((prediction - t * np.sqrt(var), 0))
+    upperbound = np.min((1, prediction + t * np.sqrt(var)))
+    if give_prediction:
+        return prediction, [lowerbound, upperbound]
+    else:
+        return [lowerbound, upperbound]
+
+
+def CI_dropout(dropout_model, x, B=300, alpha=0.05, give_prediction=True):
+    """Construct a CI using MC-dropout.
+
+    Arguments:
+        dropout_model: A model containing dropout in the dense layers. Dropout
+            must remain active after training.
+        x: The input for which a CI is constructed.
+        B (int): The number of forward passes that is used.
+        alpha (float): The confidence level of the CI. An alpha of 0.05 corresponds
+            to a 95% CI.
+        give_prediction (Boolean): Determines if the average of the predictions
+            is also returned.
+    Return:
+        prediction: The average of the forward passes
+        lowerbound, upperbound: The lower- and upperbound of the CI.
+    """
+    predictions = [sigmoid(dropout_model(np.array([x]), training=1)) for _ in range(B)]
+    prediction = np.mean(predictions)
+    lowerbound = np.percentile(predictions, 100*(alpha/2))
+    upperbound = np.percentile(predictions, 100*(1-alpha/2))
+    if give_prediction:
+        return prediction, [lowerbound, upperbound]
+    else:
+        return [lowerbound, upperbound]
