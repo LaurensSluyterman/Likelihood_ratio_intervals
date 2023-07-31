c4d6e628dd93400927426ad527a5de103b96dd3f
LaurensSluyterman <Laurens.sluyterman@hotmail.com>
2023-07-31 14:14:14 +0200

    Changed step to delta to match notation in paper

--- intervals_regression.py
+++ intervals_regression.py
@@ -1,11 +1,10 @@
 import numpy as np
 import gc
-import scipy
 from scipy import stats
 
 
 def CI_NN(MVE_network, X, X_train, Y_train, alpha, n_steps=10,
-          n_epochs=40, step=1, fraction=0.1):
+          n_epochs=40, delta=1, fraction=0.1):
     """Calculate a confidence interval for all x values in X.
 
     This function is basically a wrapper function that calls
@@ -21,11 +20,11 @@ def CI_NN(MVE_network, X, X_train, Y_train, alpha, n_steps=10,
                                         Y_train=Y_train,
                                         mu_hats=mu_hats,
                                         sigma_hats=sigma_hats,
-                                        step=step,
+                                        delta=delta,
                                         fraction=fraction,
                                         n_steps=n_steps,
                                         n_epochs=n_epochs,
-                                        alpha=alpha,)
+                                        alpha=alpha, )
         CI[j, 0] = lowerbound
         CI[j, 1] = upperbound
     gc.collect()
@@ -33,14 +32,14 @@ def CI_NN(MVE_network, X, X_train, Y_train, alpha, n_steps=10,
 
 
 def CI_NNx(*, MVE_network, x, X_train, Y_train, mu_hats, sigma_hats,
-           n_steps, alpha, n_epochs, step, fraction):
+           n_steps, alpha, n_epochs, delta, fraction):
     # Step 1: Let the network train more to reach different values at x.
     x = np.array([x])
     start_value = MVE_network.f(x)
     MVE_network.train_on(x_new=x, X_train=X_train, Y_train=Y_train,
-                         step=step, n_epochs=n_epochs, fraction=fraction)
+                         delta=delta, n_epochs=n_epochs, fraction=fraction)
     MVE_network.train_on(x_new=x, X_train=X_train, Y_train=Y_train,
-                         step=step, positive=0, n_epochs=n_epochs, fraction=fraction)
+                         delta=delta, positive=0, n_epochs=n_epochs, fraction=fraction)
     max_value = MVE_network.f_perturbed(x, positive=True)
     min_value = MVE_network.f_perturbed(x, positive=False)
 
@@ -57,7 +56,7 @@ def CI_NNx(*, MVE_network, x, X_train, Y_train, mu_hats, sigma_hats,
         if start_value > max_value:
             break
         # Create a new alternative
-        mu_tildes = l * perturbed_predictions_positive + (1-l)*mu_hats
+        mu_tildes = l * perturbed_predictions_positive + (1-l) * mu_hats
         # Check if we can accept this
         accepting = accept(Y_train, mu_tildes, mu_hats, sigma_hats, alpha)
         # Update the upperbound if we accept
@@ -93,3 +92,4 @@ def accept(Y, mu_0, mu_1, sigma, alpha):
 def loglikelihoodratio(y, mu_0, mu_hat, sigma):
     """Return the log of the likelihood ratio"""
     return -(- 0.5 * np.sum(((y - mu_hat) / sigma)**2) + 0.5 * np.sum(((y - mu_0) / sigma)**2))
+
