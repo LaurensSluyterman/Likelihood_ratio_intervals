3117632f5aa43f2313cb3aedfd28ded4a1b72dd9
LaurensSluyterman <Laurens.sluyterman@hotmail.com>
2024-05-21 12:48:42 +0200

    Added mse-regression option

--- intervals_regression.py
+++ intervals_regression.py
@@ -1,7 +1,7 @@
 import numpy as np
 import gc
-from scipy import stats
-
+import scipy
+from intervals_regression_xgboost import ll
 
 def CI_NN(MVE_network, X, X_train, Y_train, alpha, n_steps=10,
           n_epochs=40, delta=1, fraction=0.1):
@@ -93,3 +93,56 @@ def loglikelihoodratio(y, mu_0, mu_hat, sigma):
     """Return the log of the likelihood ratio"""
     return -(- 0.5 * np.sum(((y - mu_hat) / sigma)**2) + 0.5 * np.sum(((y - mu_0) / sigma)**2))
 
+
+def CI_NNMSEx(*, MSE_network, x, X_train, Y_train, n_steps, alpha, delta,
+              n_epochs, fraction):
+    # Step 1: Let the network train more to reach different values at x.
+    critical_value = scipy.stats.chi2(1).ppf(1 - alpha)
+    x = np.array([x])
+    start_value = MSE_network.f(x)
+    mu_hats = MSE_network.f(X_train)
+    MSE_network.train_on(x_new=x, X_train=X_train, Y_train=Y_train,
+                         delta=delta, n_epochs=n_epochs, fraction=fraction)
+    MSE_network.train_on(x_new=x, X_train=X_train, Y_train=Y_train,
+                         delta=delta, positive=0, n_epochs=n_epochs, fraction=fraction)
+    max_value = MSE_network.f_perturbed(x, positive=True)
+    min_value = MSE_network.f_perturbed(x, positive=False)
+
+    # The predictions of the two perturbed networks at the training locations.
+    perturbed_predictions_positive = MSE_network.f_perturbed(X_train, positive=True)
+    perturbed_predictions_negative = MSE_network.f_perturbed(X_train, positive=False)
+
+    # Step 2: Check which linear combination of original and perturb explain the data well.
+    upperbound = start_value
+    lowerbound = start_value
+    accepting = True
+    l = 1 / n_steps
+    while accepting:
+        if start_value > max_value:
+            break
+        # Create a new alternative
+        mu_tildes = l * perturbed_predictions_positive + (1-l) * mu_hats
+        # Check if we can accept this
+        ratio = 2 * (ll(Y_train, mu_hats) - ll(Y_train, mu_tildes))
+        if ratio > critical_value:
+            accepting = False
+        # Update the upperbound if we accept
+        if accepting:
+            upperbound = start_value * (1-l) + l * max_value
+            l += 1 / n_steps
+
+    accepting = True
+    l = 1 / n_steps
+    while accepting:
+        if start_value < min_value:
+            break
+        # Create a new alternative
+        mu_tildes = l * perturbed_predictions_negative + (1 - l) * mu_hats
+        # Check if we can accept this
+        ratio = 2 * (ll(Y_train, mu_hats) - ll(Y_train, mu_tildes))
+        if ratio > critical_value:
+            accepting = False
+        if accepting:
+            lowerbound = start_value * (1-l) + l * min_value
+            l += 1 / n_steps
+    return lowerbound, upperbound
--- mve_network.py
+++ mve_network.py
@@ -261,3 +261,187 @@ def variance_transformation(b, ker=True):
     else:
         return np.exp(b) + 1e-6
 
+
+class MSENetwork:
+    """
+    This class represents a trained Mean Variance Estimation network.
+
+    The network is trained using the negative loglikelihood as loss function
+    and output an estimate for the mean, f, and standard deviation, sigma.
+    Normalization happens by default (can be switched off). The
+    predictions are given in the original scale. Per default, no
+    warm-up is used, this can be turned on. When using warm-up, the mean
+    estimate is kept fixed during the second phase. This can be turned off, in
+    which case both the mean and variance estimate get updated.
+
+    Attributes:
+        model: The trained neural network
+
+    Methods:
+        f: An estimate of the mean function, without any normalization .
+        sigma: An estimate of the standard deviation, without any normalization.
+    """
+
+    def __init__(self, *, X, Y, n_hidden, n_epochs,
+                 reg_mean=0, batch_size=None, verbose=False,
+                 normalization=True):
+        """
+        Arguments:
+            X: The unnormalized training covariates.
+            Y: The unnormalized training targets
+            n_hidden_mean (array): An array containing the number of hidden units
+                for each hidden layer of the part of the network that gives
+                the predictions for the mean.
+            n_hidden_var (array): An array containing the number of hidden units
+                for each hidden layer of the part of the network that gives
+                the predictions for the variance.
+            n_epochs (int): The number of training epochs
+            reg_mean: The regularization constant for the part of the network
+                that gives the predictions for the mean. By default, no
+                regularization is used.
+            reg_var: The regularization constant for the part of the network
+                that gives the predictions for the variance. By default, no
+                regularization is used.
+            batch_size (int): The used batch size for training, if set to None
+                the standard size of 32 is used.
+            verbose (bool): Determines if training progress is printed.
+            normalization (bool): Determines if the covariates and targets
+                are normalized before training. This normalization is
+                reversed for the final predictions.
+        """
+        self._normalization = normalization
+
+        if normalization is True:
+            self._X_mean = np.mean(X, axis=0)
+            self._X_std = np.std(X, axis=0)
+            self._Y_mean = np.mean(Y, axis=0)
+            self._Y_std = np.std(Y, axis=0)
+            X = normalize(X)
+            Y = normalize(Y)
+        model = train_network_mse(X_train=X, Y_train=Y, n_hidden=n_hidden,
+                              reg_mean=reg_mean,
+                              batch_size=batch_size,
+                              n_epochs=n_epochs,
+                              verbose=verbose,)
+        self.model = model
+
+    def f(self, X_test):
+        """Return the mean prediction"""
+        if self._normalization is True:
+            X_test = normalize(X_test, self._X_mean, self._X_std)
+            predictions = self.model.predict(X_test, verbose=0)[:, 0]
+            _ = gc.collect()  # Alleviates weird memory leak
+            return reverse_normalized(predictions, self._Y_mean, self._Y_std)
+        else:
+            predictions = self.model.predict(X_test, verbose=0)[:, 0]
+            _ = gc.collect()  # Alleviates weird memory leak
+            return predictions
+
+    def train_on(self, *, x_new, X_train, Y_train, positive=True, delta=1,
+                 batch_size=None, n_epochs=40, verbose=False, fraction=1/16,
+                 weight=1):
+        if self._normalization is True:
+            x_new = normalize(x_new, self._X_mean, self._X_std)
+            X_train = normalize(X_train, self._X_mean, self._X_std)
+            Y_train = normalize(Y_train, self._Y_mean, self._Y_std)
+        model_original = self.model
+
+        # Freeze the variance layers
+        model_original.save('./models/tempmodel.h5', overwrite=True)
+        model = tf.keras.models.load_model('./models/tempmodel.h5')
+        optimizer = tf.keras.optimizers.Adam(clipvalue=0.1, learning_rate=1e-4)
+        optimizer = tf.keras.optimizers.Adam()
+        model.compile(loss='mse', optimizer=optimizer)
+        N = len(Y_train)
+        if positive:
+            y_new = model.predict(x_new, verbose=0)[:, 0] + delta
+        else:
+            y_new = model.predict(x_new, verbose=0)[:, 0] - delta
+        x_new = np.reshape(x_new, np.shape(X_train[0]))
+        y_new = np.reshape(y_new, np.shape(Y_train[0]))
+        N_extra = int(N * fraction)
+        x_new_multiple_copies = np.array([x_new for _ in range(N_extra)])
+        y_new_multiple_copies = np.array([y_new for _ in range(N_extra)])
+        Y_train_new = np.hstack((model.predict(X_train, verbose=0)[:, 0], y_new_multiple_copies))
+        X_train_new = np.vstack((X_train, x_new_multiple_copies))
+        sample_weights = np.hstack((np.ones(len(Y_train)), weight / N_extra * np.ones(N_extra)))
+        model.fit(X_train_new, Y_train_new, batch_size=batch_size, epochs=n_epochs,
+                  verbose=verbose, sample_weight=sample_weights)
+        if positive:
+            self.positive_model = model
+        else:
+            self.negative_model = model
+
+    def f_perturbed(self, X_test, positive=True):
+        """Return the mean prediction of perturbed model"""
+        if positive:
+            perturbed_model = self.positive_model
+        else:
+            perturbed_model = self.negative_model
+        if self._normalization is True:
+            X_test = normalize(X_test, self._X_mean, self._X_std)
+            predictions = perturbed_model.predict(X_test, verbose=0)[:, 0]
+            _ = gc.collect()  # Alleviates weird memory leak
+            return reverse_normalized(predictions, self._Y_mean, self._Y_std)
+        else:
+            predictions = perturbed_model.predict(X_test, verbose=0)[:, 0]
+            _ = gc.collect()  # Alleviates weird memory leak
+            return predictions
+
+def train_network_mse(*, X_train, Y_train, n_hidden, n_epochs, reg_mean=0,
+                      batch_size=None, verbose=False):
+    """Train a network that outputs the mean and standard deviation.
+
+    This function trains a network that outputs the mean and standard
+    deviation. The network is trained using the negative loglikelihood
+    of a normal distribution as the loss function.
+
+        Arguments:
+            X_train: The training covariates.
+            Y_train: The training targets
+            n_hidden_mean (array): An array containing the number of hidden units
+                for each hidden layer of the part of the network that gives
+                the predictions for the mean.
+            n_hidden_var (array): An array containing the number of hidden units
+                for each hidden layer of the part of the network that gives
+                the predictions for the variance.
+            n_epochs (int): The number of training epochs
+            loss: The loss function
+            reg_mean: The regularization constant for the part of the network
+                that gives the predictions for the mean. By default, no
+                regularization is used.
+            reg_var: The regularization constant for the part of the network
+                that gives the predictions for the variance. By default, no
+                regularization is used.
+            batch_size (int): The used batch size for training, if set to None
+                the standard size of 32 is used.
+            verbose (bool): Determines if training progress is printed.
+
+    Returns:
+        model: A trained network that outputs a mean and log of standard
+            deviation.
+    """
+    try:
+        input_shape = np.shape(X_train)[1]
+    except IndexError:
+        input_shape = 1
+    optimizer = tf.keras.optimizers.Adam(clipvalue=0.1, learning_rate=1e-4)
+    optimizer = tf.keras.optimizers.Adam()
+    inputs = Input(shape=(input_shape,))
+    inter_mean = Dense(n_hidden[0], activation='elu',
+                       kernel_regularizer=l2(reg_mean),
+                       name='mean')(inputs)
+    for i in range(len(n_hidden) - 1):
+        inter_mean = Dense(n_hidden[i + 1], activation='elu',
+                           kernel_regularizer=l2(reg_mean),
+                           name=f'mean{i}')(inter_mean)
+    output = Dense(1, activation='linear',
+                        kernel_regularizer=l2(reg_mean),
+                        name='meanout')(inter_mean)
+    model = Model(inputs, output)
+
+    model.compile(loss='mse', optimizer=optimizer)
+    model.fit(X_train, Y_train, batch_size=batch_size, epochs=n_epochs,
+               verbose=verbose)
+    return model
+
